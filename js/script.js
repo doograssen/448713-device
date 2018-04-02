13399053e15b62deb5630bdc123c7ffa1df9dec3
Alexander <30376452+doograssen@users.noreply.github.com>
2018-04-02 21:37:06 +0300

    Правки по замечаниям

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ var showModalBtn = document.querySelector('.about__link--feedback');
 var showMap = document.querySelector('.about__map');
 var modal = document.querySelectorAll('.modal');
 var feedBack = document.querySelector('.modal--feedback');
+var userName = feedBack.querySelector('[name="user-name"]');
+var email = feedBack.querySelector('[name="user-mail"]');
+var letter = feedBack.querySelector('[name="user-feedback"]');
+var storageName = localStorage.getItem("userName");
+var storageEmail = localStorage.getItem("userEmail");
 var interactiveMap = document.querySelector('.modal--map');
 var closeFeedbackBtn = feedBack.querySelector('.modal__close');
 var closeMapBtn = interactiveMap.querySelector('.modal__close');
@@ -12,9 +17,41 @@ var googleMap = interactiveMap.querySelector('.modal__google-map');
 var mapLink = interactiveMap.querySelector('.modal__link');
 var map;
 
+function checkStorageField(check, source, target) {
+  if (check) {
+    source.value = check;
+    target.focus();
+    return true;
+  } else {
+    source.focus();
+    return false;
+  }
+}
+
+function checkField(field) {
+  if (!field.value) {
+    field.classList.add('feedback__error');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function clearoOnClose() {
+  var errorFields = feedBack.querySelectorAll('input,textarea');
+  errorFields.forEach(function (item) {
+    item.classList.remove('feedback__error');
+    item.value = '';
+  });
+  feedBack.classList.toggle('modal--show');
+}
+
 showModalBtn.addEventListener('click', function (evt) {
   evt.preventDefault();
   feedBack.classList.toggle('modal--show');
+  if (checkStorageField(storageName, userName, email)) {
+    checkStorageField(storageEmail, email, letter);
+  }
 });
 
 showMap.addEventListener('click', function (evt) {
@@ -22,12 +59,10 @@ showMap.addEventListener('click', function (evt) {
   interactiveMap.classList.toggle('modal--show');
 });
 
-closeFeedbackBtn.addEventListener('click', function (evt) {
-  evt.currentTarget.parentNode.classList.toggle('modal--show');
-});
+closeFeedbackBtn.addEventListener('click', clearoOnClose);
 
-closeMapBtn.addEventListener('click', function (evt) {
-  evt.currentTarget.parentNode.classList.toggle('modal--show');
+closeMapBtn.addEventListener('click', function () {
+  interactiveMap.classList.toggle('modal--show');
 });
 
 window.addEventListener('keydown', function (evt) {
@@ -35,6 +70,10 @@ window.addEventListener('keydown', function (evt) {
     var modalCount = modal.length;
     for (var i = 0; i < modalCount; i++) {
       if (modal[i].classList.contains('modal--show')) {
+        if (modal[i].classList.contains('modal--feedback')) {
+          clearoOnClose();
+          return;
+        }
         modal[i].classList.remove('modal--show');
         return;
       }
@@ -42,20 +81,29 @@ window.addEventListener('keydown', function (evt) {
   }
 });
 
-mapLink.addEventListener('click', function (evt) {
+feedBack.addEventListener('submit', function (evt) {
+  if (checkField(userName) || checkField(email) || checkField(letter)) {
     evt.preventDefault();
+  } else {
+    localStorage.setItem('userName', userName.value);
+    localStorage.setItem('userEmail', email.value);
+  }
+});
+
+mapLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
 });
 
 function initMap() {
-    var myLatLng = {lat: 55.687239, lng: 37.529634};
-    map = new google.maps.Map(googleMap, {
-        zoom: 17,
-        center: myLatLng
-    });
+  var myLatLng = {lat: 55.687239, lng: 37.529634};
+  map = new google.maps.Map(googleMap, {
+    zoom: 17,
+    center: myLatLng
+  });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Device Market!'
-    });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Device Market!'
+  });
 }
